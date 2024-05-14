cfd851e7ead2eedb377e68cb4adc973c146afadc
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-14 16:27:39 -0700

    Add Lesson 2.8 Practice Problem 8

---
+++ lesson_2/practice_problems/08_more_single_table_queries/q08.sql
@@ -0,0 +1,12 @@
+/**
+ * LS180 Lesson 2
+ * Assignment 8 Practice Problems: More Single Table Queries
+ * Question 8
+ *
+ * Write a SQL statement that will delete all rows from the `people` table.
+ */
+-- @block
+-- @conn residents
+-- @label delete from people
+DELETE FROM people
+;
