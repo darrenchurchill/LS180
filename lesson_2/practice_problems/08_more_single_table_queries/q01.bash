d898f4695360196165f59883d36cce81669ba27c
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-14 16:00:17 -0700

    Add Lesson 2.8 Practice Problem 1

---
+++ lesson_2/practice_problems/08_more_single_table_queries/q01.bash
@@ -0,0 +1,7 @@
+# LS180 Lesson 2
+# Assignment 8 Practice Problems: More Single Table Queries
+# Question 1
+#
+# Create a new database called `residents` using the PostgreSQL command line
+# tools.
+createdb residents
