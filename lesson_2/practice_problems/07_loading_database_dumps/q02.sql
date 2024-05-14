63b9f22531220f29c80aff3993114fc8677fd968
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-14 14:57:33 -0700

    Add Lesson 2.7 Practice Problem 2

---
+++ lesson_2/practice_problems/07_loading_database_dumps/q02.sql
@@ -0,0 +1,15 @@
+/**
+ * LS180 Lesson 2
+ * Assignment 7 Practice Problems: Loading Database Dumps
+ * Question 2
+ *
+ * Write a SQL statement that returns all rows in the `films` table.
+ */
+-- @block
+-- @conn ls180
+-- @label view all films
+SELECT
+  *
+FROM
+  films
+;
