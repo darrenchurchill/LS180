aadc797215d0158be8c7ec897f4affc94f73f7ea
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-15 13:02:02 -0700

    Add Lesson 2.11 Practice Problem 1

---
+++ lesson_2/practice_problems/11_using_keys/q01.sql
@@ -0,0 +1,12 @@
+/**
+ * LS180 Lesson 2
+ * Assignment 11 Practice Problems: Using Keys
+ * Question 1
+ *
+ * Write a SQL statement that makes a new sequence called `counter`.
+ */
+-- @block
+-- @conn ls180
+-- @label create counter sequence
+CREATE SEQUENCE counter
+;
