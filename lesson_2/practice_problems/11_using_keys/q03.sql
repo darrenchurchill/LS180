37bdec91ebc40d898c265cd0ee747b8ab61689d8
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-15 13:06:07 -0700

    Add Lesson 2.11 Practice Problem 3

---
+++ lesson_2/practice_problems/11_using_keys/q03.sql
@@ -0,0 +1,12 @@
+/**
+ * LS180 Lesson 2
+ * Assignment 11 Practice Problems: Using Keys
+ * Question 3
+ *
+ * Write a SQL statement that removes a sequence called `counter`.
+ */
+-- @block
+-- @conn ls180
+-- @label drop counter sequence
+DROP SEQUENCE IF EXISTS counter
+;
