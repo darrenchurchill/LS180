be4a57eddad062113635c1a650b49dcf3829e2a4
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-20 10:16:44 -0700

    Add Lesson 3.13 Practice Problem 7

---
+++ lesson_3/practice_problems/13_converting_a_relationship/q07.sql
@@ -0,0 +1,25 @@
+/**
+ * LS180 Lesson 3
+ * Assignment 13 Practice Problems: Converting a 1:M Relationship to a M:M Relationship
+ * Question 7
+ *
+ * Write a SQL statement that determines how many films each director in the
+ * database has directed. Sort the results by number of films (greatest first)
+ * and then name (in alphabetical order).
+ */
+-- @block
+-- @conn ls180_3_13
+-- @label directors and count of films directed
+SELECT
+  d.name director,
+  count(f.id) num_films_directed
+FROM
+  directors d
+  JOIN directors_films df ON d.id = df.director_id
+  JOIN films f ON df.film_id = f.id
+GROUP BY
+  director
+ORDER BY
+  num_films_directed DESC,
+  name
+;
